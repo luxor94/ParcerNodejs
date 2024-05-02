516f46d71c815c6b9b7d4a80d80797c6e504203b
Alexandr Popov <47752490+luxor94@users.noreply.github.com>
2024-05-02 22:23:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
-const axios = require('axios');
+const http = require('http');
 const cheerio = require('cheerio');
 
 const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "default-src 'self'");
-    next();
+  res.setHeader('Content-Security-Policy', "default-src 'self'");
+  next();
+});
+
+// Функция для отправки GET-запросов
+const getHtml = (url) => {
+  return new Promise((resolve, reject) => {
+    http.get(url, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+      response.on('end', () => {
+        resolve(data);
+      });
+    }).on('error', (error) => {
+      reject(error);
+    });
   });
-  
+};
 
 // Маршрут для получения цены по артикулу
 app.get('/price/:articleCode', async (req, res) => {
@@ -18,8 +34,7 @@ app.get('/price/:articleCode', async (req, res) => {
     const url = `https://www.wildberries.ru/catalog/${articleCode}/detail.aspx`;
 
     // Отправляем GET запрос к странице Wildberries
-    const response = await axios.get(url);
-    const html = response.data;
+    const html = await getHtml(url);
 
     // Парсим HTML с помощью Cheerio
     const $ = cheerio.load(html);
